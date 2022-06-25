50f3373e80196455c588e862f21ef37925636216
Slinky0812 <90869529+Slinky0812@users.noreply.github.com>
2022-06-25 21:23:52 +0100

    Blank canvas created

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,3 +1,26 @@
-public class Game {
-    
+import java.awt.*;
+
+
+public class Game extends Canvas implements Runnable {
+
+    //attributes
+    public static final long serialVersionUID = 1L;
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9; //gives approx 16 by 9 ratio
+
+    //constructor
+    public Game() {
+        new Window(WIDTH, HEIGHT, "Let's build a game!", this);
+    }
+
+    public synchronized void start() {
+
+    }
+
+    public void run() {
+
+    }
+
+    public static void main (String args[]) {
+        new Game();
+    }
 }
--- Game/src/Window.java
+++ Game/src/Window.java
@@ -1,3 +1,32 @@
-public class Window {
-    
+import java.awt.*;
+import javax.swing.*;
+
+public class Window extends Canvas {
+
+    private static final long serialVersionUID = 1L;
+
+    //constructor
+    public Window(int width, int height, String title, Game game) {
+
+        //created a JFrame object, which is the frame for our window
+        JFrame frame = new JFrame(title);
+
+        //set the size of the frame
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        
+        //having the close button working
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //not allowing our window to be resized
+        frame.setResizable(false);
+        //the frame starts in the middle of the screen
+        frame.setLocationRelativeTo(null);
+        //adding our game class to our frame
+        frame.add(game);
+        //making the frame visible
+        frame.setVisible(true);
+        //running the game
+        game.start();
+    }
 }
